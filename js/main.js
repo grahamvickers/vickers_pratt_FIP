aad8c1381cbeda2ad23c6a48e71911e89aeb0464
James Pratt <james.r.pratt41@gmail.com>
2019-04-08 21:17:41 -0400

    started the index mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log("javascript linked up")
+(() => {
+
+console.log("Javascript is Linked");
+
+// save shortcut to button in DOM
+var button = document.querySelector("button");
+var burgerCon = document.querySelector("#burgerCon");
+
+function hamburgerMenu() {
+    console.log("Click Worked")
+    burgerCon.classList.toggle("slideToggle");
+    button.classList.toggle("expanded");
+}
+
+button.addEventListener("click", hamburgerMenu);
+
+
+})();
